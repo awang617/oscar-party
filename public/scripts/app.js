88cb843b4bde951eb4f699df08f9d66a4d114547
ronsbons <ronnilouie@gmail.com>
2019-02-19 20:06:02 -0800

    created switch statement for categories to display actor actress or director name

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,11 +23,47 @@ function getCategoryHtml(category) {
 function getMoviesList(category) {
   let moviesArr = [];
   for (let i = 0; i < category.movies.length; i++) {
-    moviesArr.push(`<div class='nominee'><h4>${category.movies[i].name}</h4><img src='${category.movies[i].image}'></div>`);
-    // moviesArr.push(`<img src='${category.movies[i].image}'>`)
-  }
+    switch(category.name) {
+      case "Actor in a Leading Role":
+        moviesArr.push(`<div class="nominee">
+                          <h4>${category.movies[i].actor} - ${category.movies[i].name}</h4>
+                          <img src="${category.movies[i].image}">
+                        </div>`);
+        break;
+      case "Actress in a Leading Role":
+        moviesArr.push(`<div class="nominee">
+                          <h4>${category.movies[i].actress} - ${category.movies[i].name}</h4>
+                          <img src="${category.movies[i].image}">
+                        </div>`);
+        break;
+      case "Actor in a Supporting Role":
+        moviesArr.push(`<div class="nominee">
+                          <h4>${category.movies[i].supportingActor} - ${category.movies[i].name}</h4>
+                          <img src="${category.movies[i].image}">
+                        </div>`);
+        break;
+      case "Actress in a Supporting Role":
+        moviesArr.push(`<div class="nominee">
+                          <h4>${category.movies[i].supportingActress} - ${category.movies[i].name}</h4>
+                          <img src="${category.movies[i].image}">
+                        </div>`);
+        break;
+      case "Directing":
+        moviesArr.push(`<div class="nominee">
+                          <h4>${category.movies[i].director} - ${category.movies[i].name}</h4>
+                          <img src="${category.movies[i].image}">
+                        </div>`);
+        break;
+      default:
+        moviesArr.push(`<div class='nominee'>
+                          <h4>${category.movies[i].name}</h4>
+                          <img src="${category.movies[i].image}">
+                        </div>`);
+        // moviesArr.push(`<img src='${category.movies[i].image}'>`)
+    };
+  };
   return moviesArr.join('');
-}
+};
 
 function getAllCategoriesHtml(categories) {
   return categories.map(getCategoryHtml).join("");
